60dec508efe3bc17c6e29c3af3ba12b140ceecc9
evertonvssa <everton.vssa@upe.br>
2026-04-01 23:50:14 -0300

    Entrega dos exercicios do 09 até 13

--- src/exercicio_09.py
+++ src/exercicio_09.py
@@ -1,11 +1,3 @@
 def is_even(n: int) -> bool:
-    """
-    Verifica se um número é par.
-
-    Args:
-        n (int): número inteiro
-
-    Returns:
-        bool: True se for par, False caso contrário
-    """
-    pass
+    
+    return n % 2 == 0
--- src/exercicio_11.py
+++ src/exercicio_11.py
@@ -3,15 +3,5 @@ def modify_guest_list(
     unavailable: str,
     new_guest: str
 ) -> list[str]:
-    """
-    Substitui um convidado indisponível por outro.
-
-    Args:
-        guests (list[str]): lista original
-        unavailable (str): convidado a ser removido
-        new_guest (str): novo convidado
-
-    Returns:
-        list[str]: lista atualizada
-    """
-    pass
+    
+    return [new_guest if guest == unavailable else guest for guest in guests]
